54692cf0746d1c57c28727be4b143d9e2d15565a
Diogo <Diogo@GAMING-PC>
2017-04-04 19:59:11 +0100

    changed gerir equipas/jogadores size

--- Projeto/GerirEquipasJogadores.Designer.cs
+++ Projeto/GerirEquipasJogadores.Designer.cs
@@ -85,7 +85,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 261);
+            this.ClientSize = new System.Drawing.Size(271, 189);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonGerirJogadores);
