ca939b43189988097f721c72b46bdecafd48483f
LHCCM <2160435@my.ipleiria.pt>
2017-04-04 17:35:28 +0100

    ADFSDHG

--- Projeto/FormCartas.cs
+++ Projeto/FormCartas.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Projeto
 {
-    public partial class FormCartas : Form
+    public partial class Cartas : Form
     {
-        public FormCartas()
+        public Cartas()
         {
             InitializeComponent();
         }
