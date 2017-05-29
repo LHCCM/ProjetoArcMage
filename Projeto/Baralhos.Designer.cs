f3be8ea536da3d9da5c54c89f33d56e3a78ab7f6
abelardo57 <abel_campos1@hotmail.com>
2017-05-29 18:39:43 +0100

    Baralhos 0.0

--- Projeto/Baralhos.Designer.cs
+++ Projeto/Baralhos.Designer.cs
@@ -130,7 +130,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 378);
+            this.ClientSize = new System.Drawing.Size(375, 386);
             this.Controls.Add(this.buttonVoltar);
             this.Controls.Add(this.buttonEditarBaralho);
             this.Controls.Add(this.buttonApagarBaralho);
--- Projeto/EditarBaralho.Designer.cs
+++ Projeto/EditarBaralho.Designer.cs
@@ -39,13 +39,15 @@
             this.buttonVoltar = new System.Windows.Forms.Button();
             this.buttonRemoverCartaBaralho = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBoxBaralhos = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelEditarBaralho
             // 
             this.labelEditarBaralho.AutoSize = true;
             this.labelEditarBaralho.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelEditarBaralho.Location = new System.Drawing.Point(112, 9);
+            this.labelEditarBaralho.Location = new System.Drawing.Point(180, 9);
             this.labelEditarBaralho.Name = "labelEditarBaralho";
             this.labelEditarBaralho.Size = new System.Drawing.Size(127, 24);
             this.labelEditarBaralho.TabIndex = 0;
@@ -54,7 +56,7 @@
             // labelNomeBaralho
             // 
             this.labelNomeBaralho.AutoSize = true;
-            this.labelNomeBaralho.Location = new System.Drawing.Point(128, 33);
+            this.labelNomeBaralho.Location = new System.Drawing.Point(197, 33);
             this.labelNomeBaralho.Name = "labelNomeBaralho";
             this.labelNomeBaralho.Size = new System.Drawing.Size(89, 13);
             this.labelNomeBaralho.TabIndex = 1;
@@ -63,25 +65,25 @@
             // listBoxTotalCartas
             // 
             this.listBoxTotalCartas.FormattingEnabled = true;
-            this.listBoxTotalCartas.Location = new System.Drawing.Point(12, 85);
+            this.listBoxTotalCartas.Location = new System.Drawing.Point(174, 85);
             this.listBoxTotalCartas.Name = "listBoxTotalCartas";
-            this.listBoxTotalCartas.Size = new System.Drawing.Size(145, 251);
+            this.listBoxTotalCartas.Size = new System.Drawing.Size(145, 264);
             this.listBoxTotalCartas.TabIndex = 2;
             this.listBoxTotalCartas.SelectedIndexChanged += new System.EventHandler(this.listBoxTotalCartas_SelectedIndexChanged);
             // 
             // listBoxCartasBaralho
             // 
             this.listBoxCartasBaralho.FormattingEnabled = true;
-            this.listBoxCartasBaralho.Location = new System.Drawing.Point(172, 85);
+            this.listBoxCartasBaralho.Location = new System.Drawing.Point(325, 85);
             this.listBoxCartasBaralho.Name = "listBoxCartasBaralho";
-            this.listBoxCartasBaralho.Size = new System.Drawing.Size(149, 251);
+            this.listBoxCartasBaralho.Size = new System.Drawing.Size(158, 264);
             this.listBoxCartasBaralho.TabIndex = 3;
             this.listBoxCartasBaralho.SelectedIndexChanged += new System.EventHandler(this.listBoxCartasBaralho_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(180, 69);
+            this.label1.Location = new System.Drawing.Point(333, 69);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 4;
@@ -90,7 +92,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Location = new System.Drawing.Point(197, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 13);
             this.label2.TabIndex = 5;
@@ -98,16 +100,16 @@
             // 
             // textBoxProcura
             // 
-            this.textBoxProcura.Location = new System.Drawing.Point(12, 386);
+            this.textBoxProcura.Location = new System.Drawing.Point(174, 415);
             this.textBoxProcura.Name = "textBoxProcura";
-            this.textBoxProcura.Size = new System.Drawing.Size(309, 20);
+            this.textBoxProcura.Size = new System.Drawing.Size(145, 20);
             this.textBoxProcura.TabIndex = 7;
             this.textBoxProcura.TextChanged += new System.EventHandler(this.textBoxProcura_TextChanged);
             // 
             // buttonAdicionarCartaBaralho
             // 
             this.buttonAdicionarCartaBaralho.Enabled = false;
-            this.buttonAdicionarCartaBaralho.Location = new System.Drawing.Point(12, 412);
+            this.buttonAdicionarCartaBaralho.Location = new System.Drawing.Point(193, 357);
             this.buttonAdicionarCartaBaralho.Name = "buttonAdicionarCartaBaralho";
             this.buttonAdicionarCartaBaralho.Size = new System.Drawing.Size(100, 23);
             this.buttonAdicionarCartaBaralho.TabIndex = 8;
@@ -117,7 +119,7 @@
             // 
             // buttonVoltar
             // 
-            this.buttonVoltar.Location = new System.Drawing.Point(116, 412);
+            this.buttonVoltar.Location = new System.Drawing.Point(359, 386);
             this.buttonVoltar.Name = "buttonVoltar";
             this.buttonVoltar.Size = new System.Drawing.Size(99, 23);
             this.buttonVoltar.TabIndex = 9;
@@ -128,7 +130,7 @@
             // buttonRemoverCartaBaralho
             // 
             this.buttonRemoverCartaBaralho.Enabled = false;
-            this.buttonRemoverCartaBaralho.Location = new System.Drawing.Point(221, 412);
+            this.buttonRemoverCartaBaralho.Location = new System.Drawing.Point(359, 357);
             this.buttonRemoverCartaBaralho.Name = "buttonRemoverCartaBaralho";
             this.buttonRemoverCartaBaralho.Size = new System.Drawing.Size(100, 23);
             this.buttonRemoverCartaBaralho.TabIndex = 10;
@@ -138,7 +140,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 358);
+            this.button1.Location = new System.Drawing.Point(193, 386);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 11;
@@ -146,11 +148,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // listBoxBaralhos
+            // 
+            this.listBoxBaralhos.FormattingEnabled = true;
+            this.listBoxBaralhos.Location = new System.Drawing.Point(12, 85);
+            this.listBoxBaralhos.Name = "listBoxBaralhos";
+            this.listBoxBaralhos.Size = new System.Drawing.Size(156, 355);
+            this.listBoxBaralhos.TabIndex = 12;
+            this.listBoxBaralhos.SelectedIndexChanged += new System.EventHandler(this.listBoxBaralhos_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Baralhos";
+            // 
             // EditarBaralhoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 447);
+            this.ClientSize = new System.Drawing.Size(489, 447);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxBaralhos);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonRemoverCartaBaralho);
             this.Controls.Add(this.buttonVoltar);
@@ -183,5 +205,7 @@
         private System.Windows.Forms.Button buttonVoltar;
         private System.Windows.Forms.Button buttonRemoverCartaBaralho;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxBaralhos;
+        private System.Windows.Forms.Label label3;
     }
 }
